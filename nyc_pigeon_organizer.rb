bc0ae7d4d0ba478f9feedbd46614e870dd8dcba5
molotov2k <molotov2k@gmail.com>
2016-02-23 20:39:36 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  pigeon_names = []
+  
+  data.each do |key, att_lvl1|
+    att_lvl1.each do |att_lvl2, pigeons|
+      pigeon_names << pigeons
+    end
+  end
+  
+  pigeon_names.flatten.uniq.each do |name|
+    pigeon_list[name] = {}
+    data.each do |key, att_lvl1|
+      pigeon_list[name][key] = []
+      att_lvl1.each do |att_lvl2, pigeons|
+        pigeon_list[name][key] << att_lvl2.to_s if pigeons.include?(name)
+      end
+    end
+  end
+
+  pigeon_list
 end
